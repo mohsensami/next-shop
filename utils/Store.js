86c02036cef8c84e2f9ac32ae1f6b4ea5940e831
MohsenSami <mohsensami88@gmail.com>
2023-03-25 19:39:06 +0330

    Create-Cart-Page

--- utils/Store.js
+++ utils/Store.js
@@ -14,6 +14,10 @@ function reducer(state, action) {
             const cartItems = existItem ? state.cart.cartItems.map((item) => (item.name === existItem.name ? newItem : item)) : [...state.cart.cartItems, newItem];
             return { ...state, cart: { ...state.cart, cartItems } };
         }
+        case 'CART_REMOVE_ITEM': {
+            const cartItems = state.cart.cartItems.filter((item) => item.slug !== action.payload.slug);
+            return { ...state, cart: { ...state.cart, cartItems } };
+        }
         default:
             return state;
     }
