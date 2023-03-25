51e7286fb95c5819c3bc3e6158d2180b04cd9a4b
MohsenSami <mohsensami88@gmail.com>
2023-03-25 19:17:52 +0330

    Handle Add To Cart

--- README.md
+++ README.md
@@ -2,15 +2,7 @@
 
 decription
 
-## workflow
+Build ECommerce Website Like Amazon by Next.js
 
-1. Introduction
-2. Install Tools
-3. Create Next App
-4. Publish to Github
-5. Create Website Layout
-    1. create layout component
-    2. add header
-    3. add main section
-    4. add footer
-    5. add tailwind classes
+Source Code : https://github.com/mohsensami/next-shop
+Demo Website : https://shop-sami.vercel.app
--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,31 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext } from 'react';
+import { Store } from '../utils/Store';
 
 export default function Layout({ title, children }) {
+    const { state } = useContext(Store);
+    const { cart } = state;
     return (
         <>
             <Head>
-                <title>{title ? title + ' - React Shop' : 'React Shop'}</title>
+                <title>{title ? title + ' - Next Shop' : 'Next Shop'}</title>
                 <meta name="description" content="Ecommerce Website" />
-                <link rel="icon" href="/favicon.ico" />
+                <link legacyBehavior rel="icon" href="/favicon.ico" />
             </Head>
 
             <div className="flex min-h-screen flex-col justify-between ">
                 <header>
                     <nav className="flex h-12 items-center px-4 justify-between shadow-md">
                         <Link legacyBehavior href="/">
-                            <a className="text-lg font-bold">React Shop</a>
+                            <a className="text-lg font-bold">Next Shop</a>
                         </Link>
                         <div>
                             <Link legacyBehavior href="/cart">
-                                <a className="p-2">Cart</a>
+                                <a className="p-2">
+                                    Cart
+                                    {cart.cartItems.length > 0 && <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">{cart.cartItems.reduce((a, c) => a + c.quantity, 0)}</span>}
+                                </a>
                             </Link>
                             <Link legacyBehavior href="/login">
                                 <a className="p-2">Login</a>
