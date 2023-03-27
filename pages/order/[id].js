e94c203c04b05d130c061cea89e80f8d004a0997
MohsenSami <mohsensami88@gmail.com>
2023-03-27 14:46:08 +0330

    Deliver Order For Admin

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,5 +1,6 @@
 import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 import axios from 'axios';
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -25,18 +26,32 @@ function reducer(state, action) {
         case 'PAY_RESET':
             return { ...state, loadingPay: false, successPay: false, errorPay: '' };
 
+        case 'DELIVER_REQUEST':
+            return { ...state, loadingDeliver: true };
+        case 'DELIVER_SUCCESS':
+            return { ...state, loadingDeliver: false, successDeliver: true };
+        case 'DELIVER_FAIL':
+            return { ...state, loadingDeliver: false };
+        case 'DELIVER_RESET':
+            return {
+                ...state,
+                loadingDeliver: false,
+                successDeliver: false,
+            };
+
         default:
             state;
     }
 }
 function OrderScreen() {
+    const { data: session } = useSession();
     // order/:id
     const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
 
     const { query } = useRouter();
     const orderId = query.id;
 
-    const [{ loading, error, order, successPay, loadingPay }, dispatch] = useReducer(reducer, {
+    const [{ loading, error, order, successPay, loadingPay, loadingDeliver, successDeliver }, dispatch] = useReducer(reducer, {
         loading: true,
         order: {},
         error: '',
@@ -51,10 +66,13 @@ function OrderScreen() {
                 dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
             }
         };
-        if (!order._id || successPay || (order._id && order._id !== orderId)) {
+        if (!order._id || successPay || successDeliver || (order._id && order._id !== orderId)) {
             fetchOrder();
             if (successPay) {
                 dispatch({ type: 'PAY_RESET' });
+            }
+            if (successDeliver) {
+                dispatch({ type: 'DELIVER_RESET' });
             }
         } else {
             const loadPaypalScript = async () => {
@@ -70,7 +88,7 @@ function OrderScreen() {
             };
             loadPaypalScript();
         }
-    }, [order, orderId, paypalDispatch, successPay]);
+    }, [order, orderId, paypalDispatch, successDeliver, successPay]);
     const { shippingAddress, paymentMethod, orderItems, itemsPrice, taxPrice, shippingPrice, totalPrice, isPaid, paidAt, isDelivered, deliveredAt } = order;
 
     function createOrder(data, actions) {
@@ -102,6 +120,18 @@ function OrderScreen() {
     }
     function onError(err) {
         toast.error(getError(err));
+    }
+
+    async function deliverOrderHandler() {
+        try {
+            dispatch({ type: 'DELIVER_REQUEST' });
+            const { data } = await axios.put(`/api/admin/orders/${order._id}/deliver`, {});
+            dispatch({ type: 'DELIVER_SUCCESS', payload: data });
+            toast.success('Order is delivered');
+        } catch (err) {
+            dispatch({ type: 'DELIVER_FAIL', payload: getError(err) });
+            toast.error(getError(err));
+        }
     }
 
     return (
@@ -198,6 +228,14 @@ function OrderScreen() {
                                             </div>
                                         )}
                                         {loadingPay && <div>Loading...</div>}
+                                    </li>
+                                )}
+                                {session.user.isAdmin && order.isPaid && !order.isDelivered && (
+                                    <li>
+                                        {loadingDeliver && <div>Loading...</div>}
+                                        <button className="primary-button w-full" onClick={deliverOrderHandler}>
+                                            Deliver Order
+                                        </button>
                                     </li>
                                 )}
                             </ul>
