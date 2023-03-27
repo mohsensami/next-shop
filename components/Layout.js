ba627f915f048d4f2137781b6455136b9d57bd35
MohsenSami <mohsensami88@gmail.com>
2023-03-27 12:45:09 +0330

    Create Admin Dashboard

--- components/Layout.js
+++ components/Layout.js
@@ -64,6 +64,13 @@ export default function Layout({ title, children }) {
                                                 Order History
                                             </DropdownLink>
                                         </Menu.Item>
+                                        {session.user.isAdmin && (
+                                            <Menu.Item>
+                                                <DropdownLink className="dropdown-link" href="/admin/dashboard">
+                                                    Admin Dashboard
+                                                </DropdownLink>
+                                            </Menu.Item>
+                                        )}
                                         <Menu.Item>
                                             <a className="dropdown-link" href="#" onClick={logoutClickHandler}>
                                                 Logout
